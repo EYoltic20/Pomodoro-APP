b11e8e964a0bc79d7a5f133f142a631a83711750
Yoltic2 <eyolticmg@icloud.com>
2022-09-21 22:56:07 -0500

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by Emilio Y Martinez on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
